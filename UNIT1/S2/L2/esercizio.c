c726c5c5373222ba59ae96a5ef1cf2c2c1b969ff
Gioele Parla <gioele.parla@gmail.com>
2025-07-09 09:58:05 -0400

    s2l2

--- UNIT1/S2/L2/esercizio.c
+++ UNIT1/S2/L2/esercizio.c
@@ -1,24 +1,19 @@
 #include <stdio.h>
-
-
 int main() {
+    int numero1, numero2; //numeri inseriti interi 
+    int prodotto; //variabile per il prodotto
 
-printf ("ciaoooo\n");
+    printf("Calcoliamo la moltiplicazione tra 2 numeri.\n");
+
+    printf("inserisci il primo numero: ");
+    scanf("%d", &numero1); //per far inserire il primo numero
+
+    printf("inserisci il secondo numero: ");
+    scanf("%d", &numero2); //per far inserire il secondo numero
+
+    prodotto = numero1 * numero2; //per calcolare il prodotto tra i 2 numeri
+
+    printf("Il prodotto dei due numeri è: %d\n" , prodotto);
+
 return 0;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 }
